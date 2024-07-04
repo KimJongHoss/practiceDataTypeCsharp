5ea106c369fca3908a4701c76e0a1d1f465ae260
KimJongHoss <haberde97@gmail.com>
2024-07-04 10:24:16 +0900

    초기 커밋.

---
+++ practiceDataType/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practiceDataType
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            int num = -100;
+            string name = "300";
+
+            //name = num;
+
+            name = num.ToString();
+            name = 400.ToString();
+
+
+        }
+    }
+}
